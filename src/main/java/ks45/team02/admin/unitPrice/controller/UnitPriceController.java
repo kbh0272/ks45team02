ae11aaf7fa87ab2b484873668dd4eeea26f8530f
임수락 <isk9612@naver.com>
2022-12-22 15:44:35 +0900

    StockController이랑 UnitPriceController 추가했습니다.

--- src/main/java/ks45/team02/admin/unitPrice/controller/UnitPriceController.java
+++ src/main/java/ks45/team02/admin/unitPrice/controller/UnitPriceController.java
@@ -1,4 +1,21 @@
 ﻿package ks45.team02.admin.unitPrice.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("goods")
 public class UnitPriceController {
+
+    @GetMapping("/addGoodsUnitPrice")
+    public String addGoodsunitPrice(){
+        return "goods/addGoodsUnitPrice";
+    }
+
+    @GetMapping("/listGoodsUnitPrice")
+    public String listGoodsUnitPrice(){
+        return "goods/listGoodsUnitPrice";
+    }
 }
