db5c048365f86b054715d4ad36b4323da153bbd8
임수락 <isk9612@naver.com>
2022-12-22 15:16:45 +0900

    VatController은 sales_buying html 폴더에 있어서 salesBuyingController에 넣었고 나머지 2개는 엑셀파일이랑 html에 없어서 따로 만지진 않았습니다.

--- src/main/java/ks45/team02/admin/sales_buying/controller/SaleBuyingController.java
+++ src/main/java/ks45/team02/admin/sales_buying/controller/SaleBuyingController.java
@@ -1,4 +1,60 @@
 ﻿package ks45.team02.admin.sales_buying.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SaleBuyingController {
+    @RequestMapping("/sales_Buying")
+
+    @GetMapping("/addGoodsBuying")
+    public String addGoodsBuying(){
+        return "sales_Buying/addGoodsBuying";
+    }
+
+    @GetMapping("/addGoodsSales")
+    public String addGoodsSales(){
+        return "sales_Buying/addGoodsSales";
+    }
+
+    @GetMapping("GoodsBuyingPayment")
+    public String GoodsBuyingPayment(){
+        return "sales_Buying/GoodsBuyingPayment";
+    }
+    @GetMapping("GoodsSalesBuyingGroup")
+    public String GoodsSalesBuyingGroup(){
+        return "sales_Buying/GoodsSalesBuyingGroup";
+    }
+
+    @GetMapping("GoodsSalesTotalInquiry")
+    public String GoodsSalesTotalInquiry(){
+        return "sales_Buying/GoodsSalesTotalInquiry";
+    }
+    @GetMapping("listGoodsBuying")
+    public String listGoodsBuying(){
+        return "sales_Buying/listGoodsBuying";
+    }
+
+    @GetMapping("listGoodsBuyVat")
+    public String listGoodsBuyVat(){
+        return "sales_Buying/listGoodsBuyVat";
+    }
+
+    @GetMapping("listGoodsSales")
+    public String listGoodsSales(){
+        return "sales_Buying/listGoodsSales";
+    }
+
+    @GetMapping("modifyGoodsBuying")
+    public String modifyGoodsBuying(){
+        return "sales_Buying/modifyGoodsBuying";
+    }
+
+    @GetMapping("modifyGoodsSales")
+    public String modifyGoodsSales(){
+        return "sales_Buying/modifyGoodsSales";
+    }
+
 }
